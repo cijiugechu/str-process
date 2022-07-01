b8365bf8ea6218078fd2faf41a29ba49435c139f
cijiugechu <cijiugechu@gmail.com>
2022-07-01 14:59:46 +0800

    basic features

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,82 @@
-export const foo = 'foo'
+type ProcessFn = (str: string) => string
+
+const transparent: ProcessFn = str => str
+
+class StrProcess {
+    fn = transparent
+
+    constructor(fn?: ProcessFn) {
+        if (fn) {
+            this.fn = fn
+        }
+    }
+
+    /**
+     * @description the static method `of` is used to create a new `StrProcess`.
+     * @param fn ProcessFn
+     * @returns `StrProcess` instance
+     */
+    static of(fn?: ProcessFn) {
+        return new StrProcess(fn)
+    }
+
+
+    /**
+     * @description StrProcess is lazy! you can use `runWith` to evaluate it.
+     * @param value string
+     * @returns string
+     */
+    runWith(value: string) {
+        return this.fn(value)
+    }
+
+
+    /**
+     * @description you can use this method to customize your own process function.
+     * @param fn ProcessFn
+     * @returns 
+     */
+    map(fn: ProcessFn) {
+        return new StrProcess(v => fn(this.fn(v)))
+    }
+
+
+    /**
+     * @description unbox the StrProcess and get the process function inside it.
+     * @returns ProcessFn
+     */
+    unbox() {
+        return this.fn
+    }
+
+    upperCase() {
+        return this.map(v => v.toUpperCase())
+    }
+
+    lowerCase() {
+        return this.map(v => v.toLowerCase())
+    }
+
+    alphaBet() {
+        return this.map(v => v.replace(/[^a-zA-Z]/g, ''))
+    }
+
+    alphaBetWithSpace() {
+        return this.map(v => v.replace(/[^a-zA-Z\s]/g, ''))
+    }
+
+    digits() {
+        return this.map(v => v.replace(/[^0-9]/g, ''))
+    }
+
+    sliceHead(n: number) {
+        return this.map(v => v.slice(0, n))
+    }
+
+}
+
+export {
+    StrProcess,
+    transparent,
+    ProcessFn
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,78 @@
-import { test, assert } from "vitest"
-import { foo } from "../src"
+import { it, describe, expect } from "vitest"
+import { StrProcess } from "../src"
 
-test("simple", () => {
-  assert.equal(foo, "foo")
+describe('StrProcess', () => {
+  it('should be a class', () => {
+    expect(typeof StrProcess).toBe('function')
+  })
+
+  it('can be construct with `of`', () => {
+    const strProcess = StrProcess.of()
+    expect(strProcess).toBeInstanceOf(StrProcess)
+  })
+
+  it('should work with `runWith`', () => {
+    const strProcess = StrProcess.of()
+    expect(strProcess.runWith('hello')).toBe('hello')
+  })
+
+  it('should work with `map`', () => {
+    const strProcess = StrProcess.of()
+    const strProcess2 = strProcess.map(v => v.toUpperCase())
+    expect(strProcess2.runWith('hello')).toBe('HELLO')
+  })
+
+  it('should work with `unbox`', () => {
+    const strProcess = StrProcess.of()
+    const fn = strProcess.unbox()
+    expect(fn('hello')).toBe('hello')
+  })
+
+  it('should work with `upperCase`', () => {
+    const strProcess = StrProcess.of()
+    const strProcess2 = strProcess.upperCase()
+    expect(strProcess2.runWith('hello')).toBe('HELLO')
+  })
+
+  it('should work with `lowerCase`', () => {
+    const strProcess = StrProcess.of()
+    const strProcess2 = strProcess.lowerCase()
+    expect(strProcess2.runWith('HELLO')).toBe('hello')
+  })
+
+  it('should work with `alphaBet`', () => {
+    const strProcess = StrProcess.of()
+    const strProcess2 = strProcess.alphaBet()
+    expect(strProcess2.runWith('h3e5l lo')).toBe('hello')
+  })
+
+  it('should work with `alphaBetWithSpace`', () => {
+    const strProcess = StrProcess.of()
+    const strProcess2 = strProcess.alphaBetWithSpace()
+    expect(strProcess2.runWith('h3e5l lo')).toBe('hel lo')
+  })
+
+  it('should work with `digits`', () => {
+    const strProcess = StrProcess.of()
+    const strProcess2 = strProcess.digits()
+    expect(strProcess2.runWith('h3e5l lo')).toBe('35')
+  })
+
+  it('should work with `sliceHead`', () => {
+    const strProcess = StrProcess.of()
+    const strProcess2 = strProcess.sliceHead(3)
+    expect(strProcess2.runWith('hello')).toBe('hel')
+  })
+
+  it('should work with chained calls', () => {
+    const process = StrProcess
+      .of()
+      .upperCase()
+      .lowerCase()
+      .alphaBet()
+      .map(v => v.slice(0, 3))
+      .unbox()
+
+    expect(process('h e3ll5o')).toBe('hel')
+  })
 })
